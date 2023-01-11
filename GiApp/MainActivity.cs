509c69be8cbc21ed6c52fd2fc9acbb30a6607d1b
Gia <raycher@outlook.com>
2023-01-11 05:39:19 +0400

    all

--- GiApp/MainActivity.cs
+++ GiApp/MainActivity.cs
@@ -1,18 +1,59 @@
-﻿using Android.App;
+﻿using System;
+using Android.App;
+using Android.Content;
 using Android.OS;
-using Android.Support.V7.App;
-
+using Android.Widget;
+using Android.Views;
+using static Android.Views.ViewGroup;
 
 namespace GiApp
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    [Activity(Label = "GiApp", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
     {
+        private TextView _textView;
+
+        private readonly string[] _strings =
+        {
+            "Wow such App...",
+            "Button tap...",
+            "Very color...",
+            "Running App...",
+            "Very interface...",
+            "So much code...",
+            "Help me I am trapped...",
+            "Is there anybody out there!?",
+            "You are tapping the crap out of me.",
+            "Stop tapping.",
+            "Okay, tap.",
+            "This text is pre-written.",
+            "You think I’m actually talking to you?",
+            "I’m not.",
+            "I want to be smart",
+            "But I’m only",
+            "a list of strings",
+            ".apk",
+            "Lonely .apk"
+        };
+
+        private readonly Random _random = new Random();
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            _textView = FindViewById<TextView>(Resource.Id.textView1);
+            var button = FindViewById<Button>(Resource.Id.changeStringButton);
+            button.Click += ChangeString;
+            ChangeString(null, null);
+        }
+
+        private void ChangeString(object sender, EventArgs e)
+        {
+            var randomIndex = _random.Next(0, _strings.Length);
+            _textView.Text = _strings[randomIndex];
+            _textView.Gravity = GravityFlags.Center;
         }
     }
 }
